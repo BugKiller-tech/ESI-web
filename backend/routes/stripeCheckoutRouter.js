5d414df6aa3eafd96d403266c3786ab87d42fbe9
jacky <hkg328@outlook.com>
2025-04-18 17:45:04 -0400

    fixed validation issue for digital only purchase

--- backend/routes/stripeCheckoutRouter.js
+++ backend/routes/stripeCheckoutRouter.js
@@ -11,8 +11,8 @@ router.post('/create-checkout-session', bodyValidatorMiddleware(Joi.object({
     lastName: Joi.string().required(),
     email: Joi.string().required(),
     phoneNumber: Joi.string().required(),
-    shippingAddress: Joi.string().optional(),
-    zipCode: Joi.string().optional(),
+    shippingAddress: Joi.string().allow('').optional(),
+    zipCode: Joi.string().allow('').optional(),
     cartItems: Joi.any().required(),
 })), controller.createStripeCheckoutSession);
 
