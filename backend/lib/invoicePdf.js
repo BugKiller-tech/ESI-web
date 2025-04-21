4d08709b1450659cdd5549f07f530b1e3604ef18
jacky <hkg328@outlook.com>
2025-04-21 15:44:36 -0400

    disable sandbox for puppeteer

--- backend/lib/invoicePdf.js
+++ backend/lib/invoicePdf.js
@@ -321,7 +321,9 @@ const createInvoicePdfWithPuppeteer = async (order, outputPath) => {
 
     fs.writeFileSync('debug.html', html);
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch(
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    );
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: 'networkidle0' });
 
