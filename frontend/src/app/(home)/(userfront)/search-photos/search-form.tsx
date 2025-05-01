82faa9c48419e45f74b9874cbb3e32ea421b5234
jacky <hkg328@outlook.com>
2025-04-30 20:44:26 -0400

    old website link update

--- frontend/src/app/(home)/(userfront)/search-photos/search-form.tsx
+++ frontend/src/app/(home)/(userfront)/search-photos/search-form.tsx
@@ -44,7 +44,7 @@ export default ({
     }, [weeks]);
 
     const openWordpressSite = () => {
-        window.open('https://esiphoto1.com/', '_blank');
+        window.open('https://old.esiphoto1.com/', '_blank');
     }
 
     const searchForHorse = async () => {
